9a179e9b3a3763cecdd51757499eeeb41ae42794
Eric Clemmons <eric@smarterspam.com>
2026-02-23 23:05:07 -0600

    Working effect

--- apps/cloud/worker/index.ts
+++ apps/cloud/worker/index.ts
@@ -1,13 +1,41 @@
+import { Effect, Layer, Schema, } from "effect";
+import { HttpRouter, HttpServer } from "effect/unstable/http";
+import { HttpApi, HttpApiBuilder, HttpApiEndpoint, HttpApiError, HttpApiGroup, HttpApiScalar } from "effect/unstable/httpapi";
+
+const Api = HttpApi.make("api")
+  .add(
+    HttpApiGroup.make("group")
+      .add(
+        HttpApiEndpoint.get("name", "/api", {
+          "success": Schema.Struct({
+            name: Schema.String,
+          })
+        }),
+        HttpApiEndpoint.get("catchAll", "*", {
+          error: HttpApiError.NotFound
+        })
+      )
+  )
+
+const GroupLive = HttpApiBuilder.group(
+  Api,
+  "group",
+  (handlers) => handlers
+    .handle("name", () => Effect.succeed({ name: "Cloudflare" }))
+    .handle("catchAll", () => Effect.fail(new HttpApiError.NotFound()))
+)
+
+const ApiLive = HttpApiBuilder.layer(Api).pipe(
+  Layer.provide(GroupLive),
+  Layer.provide(HttpServer.layerServices),
+)
+
+const DocsLive = HttpApiScalar.layer(Api);
+
+const { handler } = HttpRouter.toWebHandler(Layer.mergeAll(ApiLive, DocsLive));
+
 export default {
   fetch(request) {
-    const url = new URL(request.url);
-
-    if (url.pathname.startsWith("/api/")) {
-      return Response.json({
-        name: "Cloudflare",
-      });
-    }
-
-    return new Response(null, { status: 404 });
-  },
+    return handler(request);
+  }
 } satisfies ExportedHandler;
